37f32f4d1a7f3b3d30199e9590d8eb966d44e78a
x620 <romanchenko@it-projects.info>
2016-08-05 12:06:50 +0500

    Docs and manifests

---
+++ mail_private/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Select recipients for messages""",
+    "summary": """Module allow select recipients for send messages""",
+    "category": "Discuss",
+    "images": [],
+    "version": "1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Pavel Romanchenko",
+    "website": "https://it-projects.info",
+    "license": "GPL-3",
+    #"price": 9.00,
+    #"currency": "EUR",
+
+    "depends": [
+        "mail",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [],
+    "qweb": [],
+    "demo": [],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
