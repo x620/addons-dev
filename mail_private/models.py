7e8f906e73b6e7c4ae6a5323ae05addef80c3753
x620 <romanchenko@it-projects.info>
2016-08-09 10:28:13 +0500

    Implement Send Private message feature

--- mail_private/models.py
+++ mail_private/models.py
@@ -1,19 +1,40 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
-
-# class mail_compose_message(models.TransientModel):
-#     _inherit = 'mail.compose.message'
+from openerp import SUPERUSER_ID
 
 
-class res_partner(models.Model):
-    _inherit = 'res.partner'
+class mail_compose_message(models.TransientModel):
+    _inherit = 'mail.compose.message'
 
-    to_send = fields.Boolean(compute="_to_send", store=True)
+    private = fields.Boolean('Log an Internal Note')
 
-    @api.one
-    def _to_send(self):
-        self.to_send = False
-        # TODO: Если пользователь среди подписантов, то True
+
+class mail_thread(models.Model):
+    _inherit = 'mail.thread'
+
+    def message_get_suggested_private_recipients(self, cr, uid, ids, context=None):
+        result = dict((res_id, []) for res_id in ids)
+        current_partner = self.pool.get('res.users').browse(cr, uid, uid, context=context).partner_id
+        if 'message_follower_ids' in self._fields:
+            for obj in self.browse(cr, SUPERUSER_ID, ids, context=context):
+                for fol_obj in obj.message_follower_ids:
+                    if current_partner != fol_obj:
+                        print('Recipient: %s' % fol_obj.display_name)
+                        result[obj.id].append((fol_obj.id, '%s<%s>' % (fol_obj.name, fol_obj.email)))
+        return result
+
+    def _message_auto_subscribe_notify(self, cr, uid, ids, partner_ids, context=None):
         pass
 
+    @api.cr_uid_ids_context
+    def message_post(self, cr, uid, thread_id, body='', subject=None, type='notification',
+                     subtype=None, parent_id=False, attachments=None, context=None,
+                     content_subtype='html', **kwargs):
+        msg_id = super(mail_thread, self).message_post(cr, uid, thread_id, body, subject, type, subtype, parent_id, attachments, context, content_subtype, **kwargs)
+        if context.get('private'):
+            message = self.pool.get('mail.message').browse(cr, uid, msg_id, context=context)
+            message.notified_partner_ids = message.partner_ids
+        return msg_id
+    
+    # TODO: При отправке через форму отправляет всем подписантам
